80d0f39cf09233875a4c24da0352be934405493d
likwotsing <likwotsing@gmail.com>
2019-11-11 23:23:38 +0800

    管理资源-回退处理

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
 
 function component() {
   var element = document.createElement('div')
   
   // lodash
   element.innerHTML = _.join(['君问归期未有期', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
-
-  console.log(Data);
 
   return element;
 }

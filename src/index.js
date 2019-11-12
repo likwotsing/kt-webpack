3bc81c31edcb368f32804103b242a313679cda4c
likwotsing <likwotsing@gmail.com>
2019-11-12 15:42:15 +0800

    创建library

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
-// import Print from './print';
+import numRef from './ref.json';
 
-function component() {
-  var element = document.createElement('div');
-
-  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  // element.onclick = Print.bind(null, 'Hello webpack!!!');
-
-  return element;
+export function numToWord(num) {
+  return _.reduce(numRef, (accum, ref) =>{
+    return ref.num === num ? ref.word : accum;
+  }, '');
 }
 
-document.body.appendChild(component())
+export function wordToNum(word) {
+  return _.reduce(numRef, (accum, ref) => {
+    return ref.word === word && word.toLowerCase() ? ref.num : accum;
+  }, -1);
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,19 @@
 const path = require('path');
-const webpack = require('webpack');
-const HTMLWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
-  plugins: [
-    new HTMLWebpackPlugin({
-      title: 'Caching'
-    }),
-    new webpack.HashedModuleIdsPlugin(),
-    new CleanWebpackPlugin()
-  ],
   output: {
-    filename: '[name].[chunkhash].js',
-    path: path.resolve(__dirname, 'dist')
+    filename: 'webpack-numbers.js',
+    path: path.resolve(__dirname, 'dist'),
+    library: 'webpackNumbers',
+    libraryTarget: 'umd'
   },
-  optimization: {
-    runtimeChunk: true,
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all'
-        }
-      }
+  externals: {
+    lodash: {
+      commonjs: 'lodash',
+      commonjs2: 'lodash',
+      amd: 'lodash',
+      root: '_'
     }
   }
 }

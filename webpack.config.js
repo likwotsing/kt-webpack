c15a9e8e174bfec86254dfc17da02357cf20de2b
likwotsing <likwotsing@gmail.com>
2019-11-12 16:42:15 +0800

    发布到npm前的init

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'webpack-numbers-likwotsing.js',
+    filename: 'webpack-numbers.js',
     path: path.resolve(__dirname, 'dist'),
     library: 'webpackNumbers',
     libraryTarget: 'umd'

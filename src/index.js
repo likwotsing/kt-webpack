ed921056e8074ae3d4384f7ce52b53ec93aad74a
likwotsing <likwotsing@gmail.com>
2019-11-12 09:29:20 +0800

    实现管理输出

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import _ from 'lodash';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div')
+  var btn = document.createElement('button')
   
   // lodash
   element.innerHTML = _.join(['君问归期未有期', 'webpack'], ' ');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn)
 
   return element;
 }

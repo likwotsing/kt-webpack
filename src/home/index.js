07d060d2944c32e2105772ad22f837b30a2e052c
likwotsing <likwotsing@gmail.com>
2020-07-27 14:14:58 +0800

    多页面打包通用方案

---
+++ src/home/index.js
@@ -0,0 +1,2 @@
+import css from '@/styles/css/index.less' // 需要在webpack里配置别名
+console.log('home.js')
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,57 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const glob = require('glob')
+
+// 多页面打包通用方案
+const setMPA = () => {
+  const entry = {}
+  const htmlwebpackplugins = []
+
+  const entryFiles = glob.sync(path.join(__dirname, './src/*/index.js'))
+  // console.log(entryFiles)
+
+  entryFiles.map((item, index) => {
+    const entryFile = entryFiles[index]
+    const match = entryFile.match(/src\/(.*)\/index\.js$/)
+    // console.log(match)
+    const pageName = match && match[1]
+
+    entry[pageName] = entryFile
+
+    htmlwebpackplugins.push(
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, `./src/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: [pageName]
+      })
+    )
+  })
+
+  return {
+    entry,
+    htmlwebpackplugins
+  }
+}
+
+const { entry, htmlwebpackplugins } = setMPA()
 
 module.exports = {
   // entry: './src/index.js',
-  entry: {
-    main: './src/index.js',
-    list: './src/list.js',
-    detail: './src/detail.js'
-  },
+  // entry: {
+  //   main: './src/index.js',
+  //   list: './src/list.js',
+  //   detail: './src/detail.js'
+  // },
+  entry,
   mode: 'development',
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist')
+  },
+  resolve: {
+    alias: {
+      '@': path.join(__dirname, './src')
+    }
   },
   module: {
     rules: [
@@ -33,20 +73,21 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'index.html',
-      chunks: ['main']
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'list.html',
-      chunks: ['list']
-    }),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      filename: 'detail.html',
-      chunks: ['detail', 'list']
-    })
+    // new HtmlWebpackPlugin({
+    //   template: './src/index.html',
+    //   filename: 'index.html',
+    //   chunks: ['main']
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: './src/index.html',
+    //   filename: 'list.html',
+    //   chunks: ['list']
+    // }),
+    // new HtmlWebpackPlugin({
+    //   template: './src/index.html',
+    //   filename: 'detail.html',
+    //   chunks: ['detail', 'list']
+    // })
+    ...htmlwebpackplugins
   ]
 }
